2aef33c15dcd1e7392c7d986626f82f99e064eaf
Anil Kumar <anilsaini0663@gmail.com>
2022-06-10 10:08:52 +0530

    first commit

---
+++ power.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int power(int a,int b)
+{
+    int ans =1;
+    for(int i =1;i<=b;i++)
+    {
+        ans = ans*a;
+    }
+    return ans;
+
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int pow =  power(a,b);
+    cout<< pow;
+    return 0;
+}
