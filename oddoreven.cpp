00967897a055f1e2a664d7c7d93c6f6516f7f811
Anil Kumar <anilsaini0663@gmail.com>
2022-06-10 10:34:19 +0530

    even or odd by & operator

---
+++ oddoreven.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int isEven(int a)
+{
+    if(a&1)
+    {
+        return 0;
+    }
+    return 1;
+
+}
+int main()
+{
+    int a;
+    cin>>a;
+    if(isEven(a))
+    {
+        cout<<"number is even ";
+    }
+    else {
+        cout<<"number is odd";
+    }
+    return 0;
+}
