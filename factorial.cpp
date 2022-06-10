7d3ea593775d66d4a4683d667b5676427aeb0608
Anil Kumar <anilsaini0663@gmail.com>
2022-06-10 11:23:45 +0530

    first commit

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int factorial(int a)
+{
+    int fact =1;
+    for(int i = 1;i<=a;i++)
+    {
+        fact*=i;
+    }
+    return fact;
+
+
+}
+int main()
+{
+    int n,r;
+    int num = factorial(n);
+    int mul = factorial(r)*factroial(n-r);
+    int ans = num/mul;
+    cout<<"the factorial of "<<n <<" "<<r<<"is" <<ans;
+}
